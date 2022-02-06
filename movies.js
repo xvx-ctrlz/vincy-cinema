ab6ade3fc61c94ddff64584eafd4ab5b1f752deb
Vincy Ho <70061275+xvx-ctrlz@users.noreply.github.com>
2022-02-06 14:20:09 +0800

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,63 @@
+ function getMovies() {
+     return [{
+             id: 1,
+             type: "upcoming",
+             thumbnail: " barbecue.jpg",
+             src: "https://courses.cs.cityu.edu.hk/cs2204/video/barbecue.mp4",
+             name: " Barbecue",
+             cast: " Delicious Meat",
+             director: " Rachel",
+             duration: 50
+         },
+         {
+             id: 2,
+             type: "upcoming",
+             thumbnail: "chocolate.jpg",
+             src: "https://courses.cs.cityu.edu.hk/cs2204/video/chocolate.mp4",
+             name: " Chocolate Cake",
+             cast: " Charlie",
+             director: " Roald Daph",
+             duration: 60
+         },
+         {
+             id: 3,
+             type: "upcoming",
+             thumbnail: "casablanca.png",
+             src: "",
+             name: "Casablanca",
+             cast: "Humphrey Bogart, Ingrid Bergmen",
+             director: "Michael Curtiz",
+             duration: 130
+         }, {
+             id: 4,
+             type: "now",
+             thumbnail: "nature.jpg",
+             src: "https://courses.cs.cityu.edu.hk/cs2204/video/nature.mp4",
+             name: "Wildlife",
+             cast: "Horsy",
+             director: "Alexandrite",
+             duration: 30
+         },
+         {
+             id: 5,
+             type: "now",
+             thumbnail: "wildlife.jpg",
+             src: "https://courses.cs.cityu.edu.hk/cs2204/video/wildlife.mp4",
+             name: " Wonders",
+             cast: " Rocks",
+             director: " Rowle",
+             duration: 45
+         },
+         {
+             id: 6,
+             type: "now",
+             thumbnail: "wonders.jpg",
+             src: "",
+             name: "Nature",
+             cast: "Parrot",
+             director: "Rachel",
+             duration: 100
+         }
+
+     ]
+ }
