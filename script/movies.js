cd0c072aeea46d8bef0f38ca4b8f421d8ca99f9c
Vincy Ho <70061275+xvx-ctrlz@users.noreply.github.com>
2022-02-06 14:24:47 +0800

    Add files via upload

---
+++ script/movies.js
@@ -0,0 +1,63 @@
+function getMovies() {
+    return [{
+            id: 1,
+            type: "now",
+            thumbnail: "casablanca.png",
+            src: "http://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.mp4",
+            name: "Cassblanca",
+            cast: "Humphrey Bogart, Ingrid Bergmen",
+            director: "Michael Curtiz",
+            duration: 130
+        },
+        {
+            id: 2,
+            type: "now",
+            thumbnail: "wildlife.png",
+            src: "http://courses.cs.cityu.edu.hk/cs2204/video/wildlife.mp4",
+            name: "Big Buck",
+            cast: "Big Bunny, Little Fox",
+            director: "Forest King",
+            duration: 100
+        },
+        {
+            id: 3,
+            type: "now",
+            thumbnail: "nature.png",
+            src: "http://courses.cs.cityu.edu.hk/cs2204/video/nature.mp4",
+            name: "Nature",
+            cast: "Parrot, birds",
+            director: "Mother Nature",
+            duration: 100
+        },
+        {
+            id: 1,
+            type: "upcoming",
+            thumbnail: "casablanca.png",
+            src: "http://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.mp4",
+            name: "Cassblanca",
+            cast: "Humphrey Bogart, Ingrid Bergmen",
+            director: "Michael Curtiz",
+            duration: 130
+        },
+        {
+            id: 2,
+            type: "upcoming",
+            thumbnail: "wildlife.png",
+            src: "http://courses.cs.cityu.edu.hk/cs2204/video/wildlife.mp4",
+            name: "Big Buck",
+            cast: "Big Bunny, Little Fox",
+            director: "Forest King",
+            duration: 100
+        },
+        {
+            id: 3,
+            type: "upcoming",
+            thumbnail: "nature.png",
+            src: "http://courses.cs.cityu.edu.hk/cs2204/video/nature.mp4",
+            name: "Nature",
+            cast: "Parrot, birds",
+            director: "Mother Nature",
+            duration: 100
+        }
+    ]
+}
